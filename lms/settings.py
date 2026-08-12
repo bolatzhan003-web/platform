1490b4a4a325b95af531d943c915e022936b8ee5
bolatzhan003-web <bolatzhan003@gmail.com>
2026-08-12 20:08:25 +0500

    Fix logout redirect - redirect to home page

--- lms/settings.py
+++ lms/settings.py
@@ -175,7 +175,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # ---------------------------------------------------------------
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
 
 # ---------------------------------------------------------------
 # Default primary key field type
